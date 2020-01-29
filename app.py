596e3463188449fbd8c84eb7c306d08e4361c873
MarzDell <chodnickamarzena@yahoo.co.uk>
2020-01-29 15:32:31 +0000

    Added folder templates and file index.html as well added index to app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'recipe_manager'
+app.config['MONGO_uri'] = 'mongodb+srv://Cat:Kot@myfirstcluster-tlipu.mongodb.net/recipe_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello world'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+    
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
