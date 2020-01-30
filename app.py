064b13e6ea384f2a1a505df7abe8f1a6513a20fd
MarzDell <chodnickamarzena@yahoo.co.uk>
2020-01-30 15:44:56 +0000

    Creating insert.html and adding to app.py, checking if is working

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import TEXT
 
 
 app = Flask(__name__)
@@ -10,12 +11,39 @@ app.config['MONGO_URI'] = 'mongodb+srv://Cat:Kot@myfirstcluster-tlipu.mongodb.ne
 
 mongo = PyMongo(app)
 
+recipes = mongo.db.recipes
+the_recipe = mongo.db.recipes
+categories = mongo.db.categories
+
+mongo.db.recipes.create_index([('$**', 'text')])
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-    
+@app.route('/view_recipe')
+def view_recipe():
+    records = list(mongo.db.recipes.find())
+    return render_template('view.html', recipes=records)
+  
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('insert.html',
+                           categories=mongo.db.categories.find())
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    recipes = mongo.db.recipes
+    payload = request.form.to_dict()
+    payload.update({'views': 0})
+    recipes.insert_one(payload)
+    return redirect(url_for('view_recipe'))
+
+
+@app.route('/stat')
+def stat():
+    return render_template('statistic.html')
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
