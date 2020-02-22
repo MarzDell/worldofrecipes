835df9c64aeec691441e198d69c6485d5bd116d3
MarzDell <chodnickamarzena@yahoo.co.uk>
2020-02-22 20:20:58 +0000

    Changing True to False in app.py

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def stat():
 if __name__ == "__main__": 
                 app.run(host=os.environ.get('IP'), 
                 port=int(os.environ.get('PORT')), 
-                debug=True)
+                debug=False)
